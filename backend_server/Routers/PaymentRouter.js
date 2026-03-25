df066e0d80fea37c6b31f7b26d691bede5a88eae
quangdung9611 <nguyenphamquangdung9611@gmail.com>
2026-03-25 10:40:38 +0700

    Sua lai payment router

--- backend_server/Routers/PaymentRouter.js
+++ backend_server/Routers/PaymentRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const PaymentController = require('../Controllers/PaymentController');
 
-// Route lưu đơn hàng (Trạng thái Pending + Sinh mã Memo)
+// 1. Route lưu đơn hàng (Trạng thái Pending + Sinh mã Memo)
 // URL: POST https://webcinema-zb8z.onrender.com/api/payment/process
 router.post('/process', PaymentController.processOrder);
+
+// 2. THÊM DÒNG NÀY: Route để chốt đơn sau khi thanh toán thành công
+// URL: POST https://webcinema-zb8z.onrender.com/api/payment/complete
+router.post('/complete', PaymentController.completePayment); 
 
 module.exports = router;
