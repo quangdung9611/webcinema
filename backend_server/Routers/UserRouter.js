f0cc440deb1d1631ee41c8aa748f6ad59c99eda9
quangdung9611 <nguyenphamquangdung9611@gmail.com>
2026-04-15 09:24:13 +0700

    Sua lai cac route duong dan

--- backend_server/Routers/UserRouter.js
+++ backend_server/Routers/UserRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../Controllers/UserController'); 
-const verifyToken = require('../Middlewares/AuthMiddleware');
 
 /**
  * ==========================================
@@ -10,36 +9,34 @@ const verifyToken = require('../Middlewares/AuthMiddleware');
  */
 
 // 1. Lấy thông tin cá nhân (Profile)
-router.get('/profile', verifyToken, userController.getUserProfile);
+router.get('/profile', userController.getUserProfile);
 
 // 2. Tự cập nhật thông tin cá nhân
-router.put('/profile/update', verifyToken, userController.updateUserProfile);
+router.put('/profile/update', userController.updateUserProfile);
 
 // 3. Lấy lịch sử giao dịch
-router.get('/booking-history', verifyToken, userController.getBookingHistory);
+router.get('/booking-history', userController.getBookingHistory);
 
-// 8. Xóa sạch lịch sử và Reset điểm (DŨNG MỚI THÊM)
-// BẮT BUỘC phải có verifyToken để lấy req.user.user_id trong Controller
-router.post('/clear-history', verifyToken, userController.clearBookingHistory);
+// 8. Xóa sạch lịch sử và Reset điểm
+router.post('/clear-history', userController.clearBookingHistory);
 
 
 /**
  * ==========================================
  * NHÓM ROUTE QUẢN TRỊ (ADMIN)
- * (Nên thêm Middleware check Admin ở đây nếu có)
  * ==========================================
  */
 
 // 4. Lấy danh sách tất cả user
-router.get('/', verifyToken, userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 
 // 5. Thêm mới user trực tiếp từ Admin
-router.post('/add', verifyToken, userController.createUser);
+router.post('/add', userController.createUser);
 
 // 6. Cập nhật user theo ID
-router.put('/update/:user_id', verifyToken, userController.updateUser);
+router.put('/update/:user_id', userController.updateUser);
 
 // 7. Xóa user khỏi hệ thống
-router.delete('/delete/:user_id', verifyToken, userController.deleteUser);
+router.delete('/delete/:user_id', userController.deleteUser);
 
 module.exports = router;
