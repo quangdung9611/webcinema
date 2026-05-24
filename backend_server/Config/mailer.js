f05fe9e4aff2c7d152c975efe07c2f272774ed8e
quangdung9611 <nguyenphamquangdung9611@gmail.com>
2026-05-24 08:13:10 +0700

    sua lai phan cau hinh mailer

--- backend_server/Config/mailer.js
+++ backend_server/Config/mailer.js
@@ -2,11 +2,6 @@ const nodemailer = require('nodemailer');
 
 console.log('📧 START MAILER');
 
-console.log({
-    EMAIL_USER: process.env.EMAIL_USER,
-    EMAIL_PASS_EXISTS: !!process.env.EMAIL_PASS
-});
-
 const transporter = nodemailer.createTransport({
 
     host: 'smtp.gmail.com',
@@ -28,24 +23,5 @@ const transporter = nodemailer.createTransport({
     }
 
 });
-
-(async () => {
-
-    try {
-
-        await transporter.verify();
-
-        console.log('✅ MAILER CONNECTED');
-
-    }
-    catch (err) {
-
-        console.log('❌ MAILER VERIFY ERROR');
-
-        console.log(err);
-
-    }
-
-})();
 
 module.exports = transporter;
