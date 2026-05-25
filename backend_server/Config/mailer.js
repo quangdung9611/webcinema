a681545f3b0ee37ba40e152cfcbd1df5a03cedaf
quangdung9611 <nguyenphamquangdung9611@gmail.com>
2026-05-25 11:03:00 +0700

    Sua lai cong Port cho mailer

--- backend_server/Config/mailer.js
+++ backend_server/Config/mailer.js
@@ -2,23 +2,18 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
-    secure: false, // Dùng cho port 587
+    port: 2525, // Đổi sang 2525
+    secure: false, 
     auth: {
-        user: process.env.BREVO_USER, // Đã đổi lại thành BREVO_USER
-        pass: process.env.BREVO_PASS  // Đã đổi lại thành BREVO_PASS
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS
     },
+    // Thêm cấu hình bắt buộc này để không bị timeout
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000,
     tls: {
         rejectUnauthorized: false
-    }
-});
-
-// TEST SMTP
-transporter.verify((err, success) => {
-    if (err) {
-        console.log('❌ BREVO SMTP ERROR:', err);
-    } else {
-        console.log('✅ BREVO SMTP READY');
     }
 });
 
