8aedf530132d4cbc8c341f1ba47e3579679fb8ad
quangdung9611 <nguyenphamquangdung9611@gmail.com>
2026-07-07 13:46:30 +0700

    Sua lai file UserRouter

--- backend_server/Routers/UserRouter.js
+++ backend_server/Routers/UserRouter.js
@@ -6,7 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../Controllers/UserController");
-const AuthMiddleware = require("../Middleware/AuthMiddleware");
+const AuthMiddleware = require("../Middlewares/AuthMiddleware");
 
 /*=========================================================
     USER ROUTES (Cần xác thực)
